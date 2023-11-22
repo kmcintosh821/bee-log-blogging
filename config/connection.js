2597ba22dfae6ed699e56af5eb0c71f16a3aab36
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-21 19:41:01 -0500

    Fixing config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const is_prod = process.env.PORT;
-
 let sequelize;
-if(is_prod) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL)
-} else sequelize = new Sequelize(
+sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
