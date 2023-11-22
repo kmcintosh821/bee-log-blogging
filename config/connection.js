e982768db76d930857170bc3a0c0a2fc02d47ea6
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-21 20:38:21 -0500

    _

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const is_prod = process.env.PORT;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
+let sequelize;
+if(is_prod) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    // Turn off SQL logging in the terminal
+    logging: false
+});
 module.exports = sequelize;
