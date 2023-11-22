c95300c2453179a76547e7688ef4b7771391addf
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-21 20:24:22 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,16 @@ require('dotenv').config();
 
 const is_prod = process.env.PORT;
 
-const sequelize = new Sequelize(
-    'bee_log_db',
-    'root',
-    '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    // Turn off SQL logging in the terminal
-    logging: false
-});
+let sequelize;
+// if(is_prod) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
+// } else sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USERNAME,
+//     process.env.DB_PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     // Turn off SQL logging in the terminal
+//     logging: false
+// });
 module.exports = sequelize;
