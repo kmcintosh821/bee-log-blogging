a34f46e7eb614b3153ad350c577ee184a2a13ce7
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-21 20:26:46 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const is_prod = process.env.PORT;
 
 let sequelize;
 if(is_prod) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL, {dialect: 'mysql'})
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {host: 'localhost', dialect: 'mysql'})
 } else sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
