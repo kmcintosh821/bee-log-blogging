db0590808649e2fdff80f704e7e1288839a015cc
kmcintosh821 <115330642+BittersweetNSour@users.noreply.github.com>
2023-11-21 20:25:40 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 const is_prod = process.env.PORT;
 
 let sequelize;
-// if(is_prod) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL)
-// } else sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USERNAME,
-//     process.env.DB_PASSWORD, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     // Turn off SQL logging in the terminal
-//     logging: false
-// });
+if(is_prod) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {dialect: 'mysql'})
+} else sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    // Turn off SQL logging in the terminal
+    logging: false
+});
 module.exports = sequelize;
